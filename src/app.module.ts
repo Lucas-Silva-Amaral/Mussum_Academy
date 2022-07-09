31ed6e17a176374d265b6f65d34c3eff287976be
Lucas_Amaral <lukinhasilvaamaral@gmail.com>
2022-07-08 22:16:01 -0300

    :wrench: Add initial config for graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [ 
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    sortSchema: true,
+    driver: ApolloDriver,
+  }),
+],
 })
 export class AppModule {}
