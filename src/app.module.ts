38112e25b4d589d9399e4bad36322dc78cb87add
Lucas_Amaral <lukinhasilvaamaral@gmail.com>
2022-07-08 23:16:31 -0300

    :wrench: Initial config for typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,18 @@ import { join } from 'path';
     sortSchema: true,
     driver: ApolloDriver,
   }),
+  ConfigModule.forRoot(),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.TYPEORM_HOST,
+    port: +process.env.TYPEORM_PORT || 5432,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+    logging: process.env.TYPEORM_LOGGING === 'true',
+  }),
 ],
 })
 export class AppModule {}
