92b3a95393e0a5cac63758e2ecf9cb0161a6559b
Lucas_Amaral <lukinhasilvaamaral@gmail.com>
2022-07-10 16:49:22 -0300

    :sparkles: Add config module for lessons

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
 import { StudentsModule } from './modules/students/students.module';
 import { DisciplinesModule } from './modules/disciplines/disciplines.module';
+import { LessonsModule } from './modules/lessons/lessons.module';
+import { ContentsModule } from './modules/contents/contents.module';
 @Module({
   imports: [ 
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -27,6 +29,8 @@ import { DisciplinesModule } from './modules/disciplines/disciplines.module';
   }),
   StudentsModule,
   DisciplinesModule,
+  LessonsModule,
+  ContentsModule,
 ],
 })
 export class AppModule {}
